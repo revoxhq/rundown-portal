1cce6a315be1baaf3f68c80be5191d422be23119
Nishal John <nishal.john24@gmail.com>
2023-09-07 00:58:37 +0530

    Finished Search Function

---
+++ src/pages/ResourceLibrary/ResourceCard.js
@@ -0,0 +1,24 @@
+import { Card, List } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+
+export const ResourceCard = ({ titleName, cardData, avatarIcon }) => {
+    return (
+
+        <Card className='resource-card' title={titleName} extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
+            <List
+                itemLayout="horizontal"
+                dataSource={cardData}
+                renderItem={(item, index) => (
+                    <List.Item style={{ textAlign: 'left' }}>
+                        <List.Item.Meta
+                            avatar={avatarIcon}
+                            title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
+                            description={<p>{item.project} - {item.description}</p>}
+                            listkey={item.name}
+                        />
+                    </List.Item>
+                )}
+            />
+        </Card>
+    );
+}
--- src/pages/ResourceLibrary/ResourceLibrary.js
+++ src/pages/ResourceLibrary/ResourceLibrary.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { db } from "../../config/firebase";
 import { getDocs, collection } from 'firebase/firestore'
-import { Button, Card, Input, List, Col, Row } from 'antd';
-import { SearchOutlined, ReadOutlined, EditOutlined, PartitionOutlined, AudioOutlined, HighlightOutlined, FolderOpenOutlined, BarChartOutlined, CoffeeOutlined } from '@ant-design/icons';
+import { Button, Input, Col, Row, AutoComplete } from 'antd';
+import { SearchOutlined, ReadOutlined, PartitionOutlined, AudioOutlined, HighlightOutlined, FolderOpenOutlined, BarChartOutlined, CoffeeOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { ResourceCard } from './ResourceCard';
 
 const { Search } = Input;
 
@@ -17,6 +18,10 @@ export const ResourceLibrary = () => {
     const [audio_data, setAudio] = useState([]);
     const [folder_data, setFolder] = useState([]);
     const [other_data, setOthers] = useState([]);
+    const [search_data, setSearchData] = useState([]);
+    const [allResourceData, setResourceData] = useState([]);
+
+    const [searchOptions, setSearchOptions] = useState([]);
 
     // const [resourceList, setResourceList] = useState([]);
     const resourceListRef = collection(db, "reference-resources");
@@ -30,6 +35,7 @@ export const ResourceLibrary = () => {
                     key: doc.id
                 }))
                 filterData(filteredRefData);
+                setResourceData(filteredRefData);
             }
             catch (err) {
                 console.error(err)
@@ -38,6 +44,68 @@ export const ResourceLibrary = () => {
 
         getRefList();
     }, []);
+
+
+    const renderTitle = (title) => (
+        <span>
+            {title}
+            <a
+                style={{
+                    float: 'right',
+                }}
+                href="https://www.google.com/search?q=antd"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                more
+            </a>
+        </span>
+    );
+    const renderItem = (title,link) => ({
+        value: title,
+        label: (
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <a href={link} target="_blank" rel="noreferrer">{title}</a>
+                {/* {title} */}
+            </div>
+        ),
+    });
+
+    const optionsCopy = [
+        {
+            label: renderTitle('Documents'),
+            options: [],
+        },
+        {
+            label: renderTitle('Worksheet'),
+            options: [],
+        },
+        {
+            label: renderTitle('Moodboard'),
+            options: [],
+        },
+        {
+            label: renderTitle('Artwork'),
+            options: [],
+        },
+        {
+            label: renderTitle('Audio'),
+            options: [],
+        },
+        {
+            label: renderTitle('Folder'),
+            options: [],
+        },
+        {
+            label: renderTitle('Other'),
+            options: [],
+        },
+    ];
 
     const filterData = (filteredData) => {
         setDocuments([]);
@@ -52,35 +120,81 @@ export const ResourceLibrary = () => {
         filteredData.forEach(data => {
             switch (data.type) {
                 case "Documentation":
-                    if (documentation_data.length < 10)
+                    if (documentation_data.length < 3)
                         setDocuments(documentation_data => [...documentation_data, data]);
                     break;
                 case "Worksheet":
-                    if (worksheet_data.length < 10)
+                    if (worksheet_data.length < 4)
                         setWorksheet(worksheet_data => [...worksheet_data, data]);
                     break;
                 case "Moodboard":
-                    if (moodboard_data.length < 10)
+                    if (moodboard_data.length < 4)
                         setMoodboard(moodboard_data => [...moodboard_data, data]);
                     break;
                 case "Artwork":
-                    if (artwork_data.length < 10)
+                    if (artwork_data.length < 4)
                         setArtwork(artwork_data => [...artwork_data, data]);
                     break;
                 case "Audio":
-                    if (audio_data.length < 10)
+                    if (audio_data.length < 4)
                         setAudio(audio_data => [...audio_data, data]);
                     break;
                 case "Folder":
-                    if (folder_data.length < 10)
+                    if (folder_data.length < 4)
                         setFolder(folder_data => [...folder_data, data]);
                     break;
                 default:
-                    if (other_data.length < 10)
+                    if (other_data.length < 4)
                         setOthers(other_data => [...other_data, data]);
                     break;
             }
         });
+
+    }
+
+    const onSearching = (e) => {
+        setSearchData([]);
+        allResourceData.forEach(data => {
+            if ((data.name).toLowerCase().includes(e.toLowerCase())) {
+                setSearchData(search_data => [...search_data, data]);
+            }
+            else if ((data.description).toLowerCase().includes(e.toLowerCase())) {
+                setSearchData(search_data => [...search_data, data]);
+            }
+        });
+        populateSearch(search_data);
+
+    }
+    const populateSearch = (searchdata) => {
+        searchdata.forEach(data => {
+            let index = 6;
+            switch (data.type) {
+                case "Documentation":
+                    index = 0;
+                    break;
+                case "Worksheet":
+                    index = 1;
+                    break;
+                case "Moodboard":
+                    index = 2;
+                    break;
+                case "Artwork":
+                    index = 3;
+                    break;
+                case "Audio":
+                    index = 4;
+                    break;
+                case "Folder":
+                    index = 5;
+                    break;
+                default:
+                    index = 6;
+                    break;
+            }
+            optionsCopy[index].options.push(renderItem(data.name, data.link));
+        });
+        setSearchOptions(optionsCopy);
+
     }
 
     return (
@@ -90,149 +204,66 @@ export const ResourceLibrary = () => {
                 <Button icon={<SearchOutlined />}>Add New Resource</Button>
             </Link>
 
-            <Search placeholder="Input search text" enterButton="Search" size="large" style={{ width: 800 }} />
+
+            <AutoComplete
+                popupClassName="certain-category-search-dropdown"
+                dropdownMatchSelectWidth={500}
+                style={{
+                    width: 800,
+                }}
+                options={searchOptions}
+            >
+                {/* <Input.Search size="large" placeholder="input here" /> */}
+                <Search placeholder="Input search text" enterButton="Search" size="large" style={{ width: 800 }} onChange={e => onSearching(e.target.value)} />
+
+            </AutoComplete>
+
+
+
 
 
             <Row gutter={[16, 24]} className='justify-center m-t-50'>
 
                 <Col className="gutter-row" span={5}>
                     {/* Documentation */}
-                    <Card className='resource-card' title="Documentation" extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={documentation_data}
-                            renderItem={(item, index) => (
-                                <List.Item style={{ textAlign: 'left' }}>
-                                    <List.Item.Meta
-                                        avatar={<ReadOutlined />}
-                                        title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
-                                        description={<p>{item.project} - {item.description}</p>}
-                                        listkey={item.name}
-                                    />
-                                </List.Item>
-                            )}
-                        />
-                    </Card>
+                    <ResourceCard titleName={"Documentation"} cardData={documentation_data} avatarIcon={<ReadOutlined />} />
                 </Col>
 
                 <Col className="gutter-row" span={5}>
                     {/* Moodboard */}
-                    <Card className='resource-card' title="Moodboards" extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={moodboard_data}
-                            renderItem={(item, index) => (
-                                <List.Item style={{ textAlign: 'left' }}>
-                                    <List.Item.Meta
-                                        avatar={<PartitionOutlined />}
-                                        title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
-                                        description={<p>{item.project} - {item.description}</p>}
-                                        listkey={item.name}
-                                    />
-                                </List.Item>
-                            )}
-                        />
-                    </Card>
+                    <ResourceCard titleName={"Moodboards"} cardData={moodboard_data} avatarIcon={<PartitionOutlined />} />
+
                 </Col>
 
                 <Col className="gutter-row" span={5}>
                     {/* Audio */}
-                    <Card title="Audio" extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={audio_data}
-                            renderItem={(item, index) => (
-                                <List.Item style={{ textAlign: 'left' }}>
-                                    <List.Item.Meta
-                                        avatar={<AudioOutlined />}
-                                        title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
-                                        description={<p>{item.project} - {item.description}</p>}
-                                        listkey={item.name}
-                                    />
-                                </List.Item>
-                            )}
-                        />
-                    </Card>
+                    <ResourceCard titleName={"Audio"} cardData={audio_data} avatarIcon={<AudioOutlined />} />
+
+
                 </Col>
 
                 <Col className="gutter-row" span={5}>
                     {/* Artwork */}
-                    <Card title="Artwork" extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={artwork_data}
-                            renderItem={(item, index) => (
-                                <List.Item style={{ textAlign: 'left' }}>
-                                    <List.Item.Meta
-                                        avatar={<HighlightOutlined />}
-                                        title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
-                                        description={<p>{item.project} - {item.description}</p>}
-                                        listkey={item.name}
-                                    />
-                                </List.Item>
-                            )}
-                        />
-                    </Card>
+                    <ResourceCard titleName={"Artwork"} cardData={artwork_data} avatarIcon={<HighlightOutlined />} />
+
                 </Col>
 
                 <Col className="gutter-row" span={5}>
                     {/* Folder */}
-                    <Card title="Folders" extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={folder_data}
-                            renderItem={(item, index) => (
-                                <List.Item style={{ textAlign: 'left' }}>
-                                    <List.Item.Meta
-                                        avatar={<FolderOpenOutlined />}
-                                        title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
-                                        description={<p>{item.project} - {item.description}</p>}
-                                        listkey={item.name}
-                                    />
-                                </List.Item>
-                            )}
-                        />
-                    </Card>
+                    <ResourceCard titleName={"Folders"} cardData={folder_data} avatarIcon={<FolderOpenOutlined />} />
+
                 </Col>
 
                 <Col className="gutter-row" span={5}>
                     {/* Worksheet */}
-                    <Card title="Worksheet" extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={worksheet_data}
-                            renderItem={(item, index) => (
-                                <List.Item style={{ textAlign: 'left' }}>
-                                    <List.Item.Meta
-                                        avatar={<BarChartOutlined />}
-                                        title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
-                                        description={<p>{item.project} - {item.description}</p>}
-                                        listkey={item.name}
-                                    />
-                                </List.Item>
-                            )}
-                        />
-                    </Card>
+                    <ResourceCard titleName={"Worksheet"} cardData={worksheet_data} avatarIcon={<BarChartOutlined />} />
+
                 </Col>
 
                 <Col className="gutter-row" span={5}>
                     {/* Other */}
-                    <Card title="Other" extra={<a href="#">More</a>} style={{ width: 300 }} hoverable={true}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={other_data}
-                            renderItem={(item, index) => (
-                                <List.Item style={{ textAlign: 'left' }}>
-                                    <List.Item.Meta
-                                        avatar={<CoffeeOutlined />}
-                                        title={<div className='d-flex justify-space-between'><a href={item.link} target="_blank" rel="noreferrer">{item.name}</a><a href={item.link}><EditOutlined /></a></div>}
-                                        description={<p>{item.project} - {item.description}</p>}
-                                        listkey={item.name}
-                                    />
-                                </List.Item>
-                            )}
-                        />
-                    </Card>
+                    <ResourceCard titleName={"Other"} cardData={other_data} avatarIcon={<CoffeeOutlined />} />
+
                 </Col>
             </Row>
         </div>
